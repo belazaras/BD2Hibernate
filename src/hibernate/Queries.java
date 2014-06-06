60a317601a789a4fea484bef20dbc280413801e0
Giuliano <delapenna.g@gmail.com>
2014-06-06 18:00:12 -0300

    Más cosas, pero que no andan

--- src/hibernate/Queries.java
+++ src/hibernate/Queries.java
@@ -1,7 +1,7 @@
 package hibernate;
 
+import java.text.*;
 import java.util.*;
-
 
 import org.hibernate.Query;
 import org.hibernate.Session;
@@ -19,7 +19,7 @@ public class Queries {
 	private static ServiceRegistry serviceRegistry;
 	private static Session session = null;
 	
-	public static void main(String[] args)  {
+	public static void main(String[] args) throws ParseException  {
 		//Session session = null;
 		try {
 			Configuration cfg = new Configuration();
@@ -33,7 +33,9 @@ public class Queries {
 			}
         listarSeries();
         seriesConSecuencia("Sim");
-        peliMasVista("2013");
+        //peliMasVista("2013");
+        //usuariosMasPelis(10);
+        usuariosMenos65();
 		}
 	
 	private static void listarSeries() {
@@ -88,19 +90,21 @@ public class Queries {
 		
 	}	
 	
-	private static void peliMasVista(String year) { //FALTA TERMINAR
-		String queryString = "select max(p.) from Reproduccion r where r.esPelicula() and YEAR(r.getFecha()) like :year";
+	/**private static void peliMasVista(String year) throws ParseException { //FALTA TERMINAR
+		String queryString = "select count(p.id) from Reproduccion r  where r.reproducible.class = 'Pelicula' and r.fecha between :ini and :fin";
+		DateFormat df = new SimpleDateFormat("dd-MM-yyyy");
+		Date fechaIni = df.parse("01-01-"+year);
+		Date fechaFin = df.parse("31-12-"+year);
 		session = sessionFactory.openSession();
-		
 		try	{
 		  Transaction tx = session.beginTransaction();
-		  Pelicula peli = (Pelicula) session.createQuery(queryString).setString("year", "%"+year+"%").setMaxResults(1).uniqueResult();
+		  Pelicula peli = (Pelicula) session.createQuery(queryString).setDate("ini", fechaIni).setDate("fin", fechaFin).setMaxResults(1).uniqueResult();
 		  tx.rollback();
 		  System.out.println("----------------------------------------------------------------------");
 		  System.out.println("Informar la película más vista en un determinado año (donde el año es parametrizable). Imprimir en consola: \"El título de la Película más vista en el año: \"...\" es: \"");
 		  System.out.println("Parámetro: \""+ year +"\"");
 		  System.out.println("----------------------------------------------------------------------");
-
+		  System.out.println("El título de la Película más vista en el año: "+year+" es: "+ peli.getTitulo());
 		  System.out.println("----------------------------------------------------------------------");
 		  } 
 		  catch (Exception e) {
@@ -111,5 +115,59 @@ public class Queries {
 		    }
 		
 	}	
+	
+	private static void usuariosMasPelis(int n) {
+		String queryString = "select U.email, count(U.id) from Usuario U, Reproduccion R where U.gestor.reproducciones>=:n and R.reproducible = 'h'";
+		session = sessionFactory.openSession();
+		Query seriesSec = session.createQuery(queryString).setInteger("n", n);
+		try	{
+		  Transaction tx = session.beginTransaction();
+		  Iterator<?> s = seriesSec.iterate();
+		  tx.rollback();
+		  System.out.println("----------------------------------------------------------------------");
+		  System.out.println("Listar las series cuyo título contenga una secuencia de caracteres (la secuencia es un parámetro). Imprimir en consola: \"Título de la Serie: \"");
+		  //System.out.println("Parámetro: \""+ sec +"\"");
+		  System.out.println("----------------------------------------------------------------------");
+		  while (s.hasNext()) {
+			Serie serie = (Serie) s.next();
+			System.out.println("Título de la Serie: " + serie.getTitulo());  
+		    }
+		  System.out.println("----------------------------------------------------------------------");
+		  } 
+		  catch (Exception e) {
+		  System.out.println(e.getMessage());
+		  } 
+		  finally {
+		    session.close();
+		    }
+		
+	}**/
+	
+	private static void usuariosMenos65() { //FALTA
+		String queryString = "from Usuario U where U.getIdUsuario() = '2'";
+		session = sessionFactory.openSession();
+		Query seriesSec = session.createQuery(queryString);
+		try	{
+		  Transaction tx = session.beginTransaction();
+		  Iterator<?> s = seriesSec.iterate();
+		  tx.rollback();
+		  System.out.println("----------------------------------------------------------------------");
+		  System.out.println("Listar las series cuyo título contenga una secuencia de caracteres (la secuencia es un parámetro). Imprimir en consola: \"Título de la Serie: \"");
+		  //System.out.println("Parámetro: \""+ sec +"\"");
+		  System.out.println("----------------------------------------------------------------------");
+		  while (s.hasNext()) {
+			Serie serie = (Serie) s.next();
+			System.out.println("Título de la Serie: " + serie.getTitulo());  
+		    }
+		  System.out.println("----------------------------------------------------------------------");
+		  } 
+		  catch (Exception e) {
+		  System.out.println(e.getMessage());
+		  } 
+		  finally {
+		    session.close();
+		    }
+		
+	}		
 	
 }
